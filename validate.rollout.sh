b0940593118b803021bdb39c6eeaab6691e11013
main <rjgc2810@gmail.com>
2023-04-16 16:12:43 -0300

    Rollout

--- validate.rollout.sh
+++ validate.rollout.sh
@@ -2,6 +2,7 @@
 
 sleep 60s
 ./kubectl rollout history deployment "$APP_NAME"
+./kubectl rollout status deployment "$APP_NAME" --timeout 5s
 if [[ $(./kubectl rollout status deployment "$APP_NAME" --timeout 5s) != 'deployment "$APP_NAME" successfully rolled out' ]];
 then
 echo "Deployment $APP_NAME rollout status has failed"
